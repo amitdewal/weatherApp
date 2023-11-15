59a1f0d2dca4392e4de091516b8444dce93cbd4d
amitdewal <daredewal007@gmail.com>
2023-11-16 00:31:13 +0530

    city name check and temp in celcius

--- script.js
+++ script.js
@@ -5,20 +5,45 @@ const cityName = document.getElementById("city-name");
 const cityTime = document.getElementById("city-time");
 const cityTemp = document.getElementById("city-temp");
 
-async function getData(cityName){
-const promise = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=9eede34955db4eb6ac632556231311&q=${cityName}&aqi=yes`);
+async function getData(cityName) {
+    const response = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=9eede34955db4eb6ac632556231311&q=${cityName}&aqi=yes`);
 
-    return  await promise.json();
+    if (!response.ok) {
+        // Handle the case where the request to the API is not successful
+        console.error(`Error fetching data. Status: ${response.status}`);
+        return null; // or throw an error, depending on your requirements
+    }
+
+    const result = await response.json();
+
+    if (result.error) {
+        // Handle the case where the API indicates an error (e.g., invalid city name)
+        console.error(`Error from API: ${result.error.message}`);
+        return null; // or throw an error, depending on your requirements
+    }
+
+    return result;
 }
-button.addEventListener("click", async()=>{
+
+button.addEventListener("click", async () => {
     const value = input.value;
-    const result  = await getData(value);
-    
-    cityName.innerText = `${result.location.name}, ${result.location.region}, ${result.location.country} `;
-    cityTime.innerText = result.location.localtime;
-    cityTemp.innerText = result.current.temp_c;
-})
+    const result = await getData(value);
+
+    if (result) {
+        cityName.innerText = `${result.location.name}, ${result.location.region}, ${result.location.country}`;
+        cityTime.innerText = result.location.localtime;
+        cityTemp.innerText = result.current.temp_c+" °C";
+    } else {
+        // Handle the case where the city name is invalid
+        
+        alert("Invalid city name")
+        cityName.innerText = "Invalid city name";
+        cityTime.innerText = "";
+        cityTemp.innerText = "";
+    }
+});
+
 
 
 //
